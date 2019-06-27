a0dcc662a83cc0500972d6e155bcabc1acfa84cc
Carlos Corpus <luiscorpus182@gmail.com>
2019-06-26 21:59:21 -0500

    agregar comentarios

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
  
- 
+ // funcion reload en cambios scss 
 function style() {
  return gulp.src('scss/*.scss')
    .pipe(sass())
    .pipe(gulp.dest('app/css'))
    .pipe(browserSync.stream())
 }
- 
+ // funcion reload en cambios en js
 function watch() {
  browserSync.init({
    server: {
